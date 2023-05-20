0fcae6ee124eb8dc1f387500cbebc66d9eb7665a
Jeremy <jeremy@je12emy.com>
2023-05-19 19:16:24 -0600

    feat: add cli struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+struct Cli {
+    private_token: String,
+    project_id: String,
+    target_branch: String,
+    base_branch: String,
+    branch: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
