9c961c54ef9a85635c2f7e1b442cd1da8bc306b3
Jeremy <jeremy@je12emy.com>
2023-05-19 19:33:11 -0600

    feat: add cli info

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clap::Parser;
 
 #[allow(unused)]
 #[derive(Debug, Parser)]
+#[command(author = "Jeremy Zelaya R. <jeremy@je12emy.com>")]
+#[command(about = "Automate cutting release branches for repositories hosted on Gitlab")]
 struct Cli {
     private_token: String,
     project_id: String,
