9e2460b3d7231587c66e0c46e8b1d730a60be03f
Jeremy <jeremy@je12emy.com>
2023-05-19 21:24:29 -0600

    feat: create MR

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ struct Cli {
     project_id: String,
     branch: String,
     base_branch: String,
-    target_branch: Option<String>,
+    target_branch: String,
+    title: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -25,16 +26,30 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build()
         .unwrap();
 
-    let response = create_branch(client, args).unwrap();
+    let response = create_branch(&client, &args).unwrap();
+    println!("Branch res text: {:?}", response.text().unwrap());
 
-    println!("res text: {:?}", response.text().unwrap());
+    let response = create_pr(&client, &args).unwrap();
+    println!("PR res text: {:?}", response.text().unwrap());
+
     return Ok(());
 }
 
-fn create_branch(client: Client, args: Cli) -> Result<reqwest::blocking::Response, ReqestError> {
+fn create_branch(client: &Client, args: &Cli) -> Result<reqwest::blocking::Response, ReqestError> {
     let create_branch_endpoint = format!(
         "https://gitlab.com/api/v4/projects/{}/repository/branches?branch={}&ref={}",
         args.project_id, args.branch, args.base_branch
     );
     client.post(create_branch_endpoint).send()
+}
+
+fn create_pr(client: &Client, args: &Cli) -> Result<reqwest::blocking::Response, ReqestError> {
+    let create_pr_endpoint = format!(
+        "https://gitlab.com/api/v4/projects/{}/merge_requests?source_branch={}&target_branch={}&title={}",
+        args.project_id,
+        args.branch,
+        args.target_branch,
+        args.title
+    );
+    client.post(create_pr_endpoint).send()
 }
