564a6096361a65e0d0499873f305ab4e5be05cc5
Jeremy <jeremy@je12emy.com>
2023-05-20 20:45:40 -0600

    feat: add flags for cli args

--- src/args.rs
+++ src/args.rs
@@ -5,12 +5,16 @@ use serde::Deserialize;
 #[command(author = "Jeremy Zelaya R. <jeremy@je12emy.com>")]
 #[command(about = "Automate cutting release branches for repositories hosted on Gitlab")]
 pub struct Cli {
+    #[arg(short = 't', long = "token")]
     pub private_token: String,
+    #[arg(short = 'i', long = "id")]
     pub project_id: String,
-    pub branch: String,
+    #[arg(short = 's', long = "source")]
     pub base_branch: String,
+    #[arg(short = 'd', long = "destination")]
     pub target_branch: String,
     #[arg(default_value_t = String::from("https://gitlab.com"))]
+    #[arg(short = 'u', long = "url")]
     pub gitlab_url: String,
 }
 
