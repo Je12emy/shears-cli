14b727c3c9a94fe0422335c7f50d4da05fff51d7
Jeremy Zelaya <jzelaya@krames.com>
2023-07-27 13:47:28 -0600

    feat: add cli commands

--- src/cli.rs
+++ src/cli.rs
@@ -1,107 +1,58 @@
-use std::{error::Error, fmt::Display};
+use clap::{Parser, Args, Subcommand};
 
-use clap::Parser;
-use reqwest::{header::InvalidHeaderValue, Error as ReqwestError};
-use serde::Deserialize;
-
-#[derive(Debug, Parser, Clone)]
-#[command(author = "Jeremy Zelaya R. <jeremy@je12emy.com>")]
-#[command(about = "Automate cutting release branches for repositories hosted on Gitlab")]
-pub struct Cli {
-    #[arg(short = 't', long = "token")]
-    pub private_token: String,
-    #[arg(short = 'i', long = "id")]
-    pub project_id: String,
-    #[arg(short = 's', long = "source")]
-    pub base_branch: String,
-    #[arg(short = 'd', long = "destination")]
-    pub target_branch: String,
-    #[arg(default_value_t = String::from("https://gitlab.com"))]
-    #[arg(short = 'u', long = "url")]
-    pub gitlab_url: String,
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct CmdArgs {
+    #[clap(subcommand)]
+    pub action: ActionSubcommand,
 }
 
-#[derive(Deserialize, Debug, Clone)]
-pub struct Project {
+#[derive(Debug, Subcommand)]
+pub enum ActionSubcommand {
+    #[clap(subcommand)]
+    /// Create a resource
+    Create(CreateSubCommand),
+}
+
+#[derive(Debug, Subcommand)]
+pub enum CreateSubCommand {
+    /// Create a release branch 
+    Branch(Branch),
+    /// Create a release merge request
+    MergeRequest(MergeRequest),
+    /// A release includes creating a new branch and a merge request
+    Release(Release),
+}
+
+#[derive(Debug, Args)]
+pub struct Release {
+    #[command(flatten)]
+    pub merge_request: MergeRequest,
+}
+
+#[derive(Debug, Args)]
+pub struct Source {
+    /// URL of your Gitlab instance
+    pub gitlab_url: String,
+    /// Personal access token
+    pub access_token: String,
+}
+
+#[derive(Debug, Args)]
+pub struct Branch {
+    /// New branch name
     pub name: String,
-    pub project_id: String,
-    pub base_branch: String,
-    pub target_branch: String,
+    /// Source branch name
+    pub source: String,
 }
 
-#[derive(Deserialize, Debug, Clone)]
-pub struct Config {
-    pub private_token: String,
-    pub gitlab_url: String,
-    pub projects: Vec<Project>,
-}
-
-#[derive(Debug)]
-pub enum CliError {
-    PrivateTokenError(InvalidHeaderValue),
-    HTTPClientBuilderError(ReqwestError),
-    HTTPError(HttpError),
-    RequestError(ReqwestError),
-}
-
-#[derive(Debug)]
-pub enum HttpError {
-    Auth,
-    NotFound,
-    Server,
-    BadRequest,
-    Unexpected,
-}
-
-impl Error for HttpError {}
-impl Error for CliError {}
-
-impl Display for HttpError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            HttpError::Auth => writeln!(f, "An authentication error was encountered, please ensure your access token is correct"),
-            HttpError::NotFound => writeln!(f, "Not found error, make sure your project ID and access token credentials are correct"),
-            HttpError::Server => writeln!(f, "An internal server error was encountered, if posible get in contact with a Gitlab adiminstrator"),
-            HttpError::Unexpected => writeln!(f, "An unkown error was encountered, please report this issue"),
-            HttpError::BadRequest => writeln!(f, "Bad request, your request arguments may be invalid"),
-        }
-    }
-}
-
-impl Display for CliError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::PrivateTokenError(_) => writeln!(
-                f,
-                "You may have passed an invalid non-visible character like new line: \"\\n\""
-            ),
-            Self::HTTPClientBuilderError(_) => {
-                writeln!(f, "An error ocurred while building the HTTP client.")
-            }
-            Self::HTTPError(kind) => writeln!(f, "{}", kind),
-            Self::RequestError(kind) => writeln!(
-                f,
-                "An error ocurred while processing your request, {}",
-                kind
-            ),
-        }
-    }
-}
-
-impl std::convert::From<InvalidHeaderValue> for CliError {
-    fn from(value: InvalidHeaderValue) -> Self {
-        Self::PrivateTokenError(value)
-    }
-}
-
-impl std::convert::From<ReqwestError> for CliError {
-    fn from(value: ReqwestError) -> Self {
-        if value.is_builder() {
-            return Self::HTTPClientBuilderError(value);
-        }
-        if value.is_request() {
-            return Self::RequestError(value);
-        }
-        todo!()
-    }
+#[derive(Debug, Args)]
+pub struct MergeRequest {
+    /// Merge request title
+    pub title: String,
+    /// Target branch name
+    pub target: String,
+    /// Source branch
+    #[command(flatten)]
+    pub branch: Branch
 }
