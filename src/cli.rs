5687b82a7a96242396d55c4393bd4dafccdae151
Jeremy Zelaya <jzelaya@krames.com>
2023-07-29 00:25:58 -0600

    feat: update cli api

--- src/cli.rs
+++ src/cli.rs
@@ -1,58 +1,77 @@
-use clap::{Parser, Args, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct CmdArgs {
     #[clap(subcommand)]
     pub action: ActionSubcommand,
+
+    #[command(flatten)]
+    pub source: Source,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum ActionSubcommand {
-    #[clap(subcommand)]
     /// Create a resource
+    #[clap(subcommand)]
     Create(CreateSubCommand),
 }
 
 #[derive(Debug, Subcommand)]
 pub enum CreateSubCommand {
-    /// Create a release branch 
+    /// Create a release branch
     Branch(Branch),
     /// Create a release merge request
     MergeRequest(MergeRequest),
-    /// A release includes creating a new branch and a merge request
-    Release(Release),
-}
-
-#[derive(Debug, Args)]
-pub struct Release {
-    #[command(flatten)]
-    pub merge_request: MergeRequest,
 }
 
 #[derive(Debug, Args)]
 pub struct Source {
     /// URL of your Gitlab instance
+    #[arg(
+        global = true,
+        default_value = "https://gitlab.com",
+        short = 'u',
+        long = "url"
+    )]
     pub gitlab_url: String,
     /// Personal access token
+    #[arg(global = true, default_value = "INVALID", short, long, required = true)]
     pub access_token: String,
 }
 
 #[derive(Debug, Args)]
 pub struct Branch {
     /// New branch name
+    #[arg(short, long)]
     pub name: String,
     /// Source branch name
-    pub source: String,
+    #[arg(short, long)]
+    pub base: String,
+    // Project information
+    #[command(flatten)]
+    pub project: Project,
 }
 
 #[derive(Debug, Args)]
 pub struct MergeRequest {
     /// Merge request title
+    #[arg(short, long)]
     pub title: String,
     /// Target branch name
+    #[arg(short, long)]
     pub target: String,
     /// Source branch
+    #[arg(short, long)]
+    pub source: String,
+    // Project information
     #[command(flatten)]
-    pub branch: Branch
+    pub project: Project,
+}
+
+#[derive(Debug, Args)]
+pub struct Project {
+    /// Project ID
+    #[arg(short, long)]
+    pub id: String,
 }
